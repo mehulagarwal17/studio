3b36dd4aa5bc165f3b3baa954042723ad461b39b
Mehul Agarwal <mehulagarwal11111@gmail.com>
2025-11-05 11:45:44 +0000

    Set up a Firebase backend

---
+++ src/firebase/config.ts
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+  "projectId": "studio-1286958434-86e4a",
+  "appId": "1:88961087335:web:cec622087e9daf62e221c2",
+  "apiKey": "AIzaSyBj3RPlbQ15epfYbsb70Z7Y9tAjCJkzSTw",
+  "authDomain": "studio-1286958434-86e4a.firebaseapp.com",
+  "measurementId": "",
+  "messagingSenderId": "88961087335"
+};
