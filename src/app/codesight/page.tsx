f9585ab15b2f133e80a3610ec6ed6a22d7afd19c
Mehul Agarwal <mehulagarwal11111@gmail.com>
2025-11-06 10:52:18 +0000

    its not coming in full screen.

--- src/app/codesight/page.tsx
+++ src/app/codesight/page.tsx
@@ -2,7 +2,7 @@
 
 export default function CodeSightPage() {
   return (
-    <div className="h-[calc(100vh-6rem)] w-full -mt-4 -ml-4 sm:-ml-6">
+    <div className="absolute inset-0 top-14 sm:top-0 sm:left-60 bg-background">
       <iframe
         src="https://pythontutor.com/visualize.html#mode=edit"
         className="w-full h-full border-0"
