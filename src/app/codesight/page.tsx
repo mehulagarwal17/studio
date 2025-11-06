ee0bf06b7d3f0cc4e4c27ed7c8a849155347275e
Mehul Agarwal <mehulagarwal11111@gmail.com>
2025-11-06 10:50:25 +0000

    let it come in full screen.

--- src/app/codesight/page.tsx
+++ src/app/codesight/page.tsx
@@ -2,10 +2,10 @@
 
 export default function CodeSightPage() {
   return (
-    <div className="h-[calc(100vh-10rem)] w-full">
+    <div className="h-[calc(100vh-6rem)] w-full -mt-4 -ml-4 sm:-ml-6">
       <iframe
         src="https://pythontutor.com/visualize.html#mode=edit"
-        className="w-full h-full border rounded-lg shadow-sm"
+        className="w-full h-full border-0"
         title="Python Tutor"
       ></iframe>
     </div>
